669ed6bc19bf19171f9409fa07a62f8ed68a827c
SEJO <sead.bander@gmail.com>
2022-12-12 09:00:24 +0100

    First commit

--- src/zadaca/PomocneMetode.java
+++ src/zadaca/PomocneMetode.java
@@ -3,7 +3,7 @@ package zadaca;
 import static zadaca.PomocneVarijable.*;
 
 public class PomocneMetode {
-    static void unosUsername() {
+    static void unosUsernameIPassword2() {
         while (brojPokusajaUserName > 0) {
             System.out.println("Unesi username");
             String username = korisnickiUnos.next();
@@ -15,15 +15,12 @@ public class PomocneMetode {
                 System.out.println("Pogresno unesen username");
                 brojPokusajaUserName--;
             }
-
             if (brojPokusajaUserName == 0) {
                 System.out.println("Pogrijesili ste username 3 puta");
                 System.out.println("Pokusajte ponovo");
                 return;
             }
         }
-    }
-    static void unosPassword(){
         while (brojPokusajaPassword > 0) {
             System.out.println("Unesi password");
             String password = korisnickiUnos.next();
@@ -35,7 +32,6 @@ public class PomocneMetode {
                 System.out.println("Pogresno unesen password");
                 brojPokusajaPassword--;
             }
-
             if (brojPokusajaPassword == 0) {
                 System.out.println("Pogrijesili ste password 3 puta");
                 System.out.println("Pokusajte ponovo");
@@ -44,13 +40,13 @@ public class PomocneMetode {
         }
     }
 
-    static void unosUsernameIPassword(){
+    static void unosUsernameIPassword() {
         while (brojPokusaja > 0) {
             System.out.println("Unesi username");
             String username = korisnickiUnos.next();
             System.out.println("Unesi password");
             String password = korisnickiUnos.next();
-            if (username.equals(myUsername)&&password.equals(myPassword)) {
+            if (username.equals(myUsername) && password.equals(myPassword)) {
                 System.out.println("Hvala. Ispravno unesen username i password");
                 System.out.println("Dobro dosli");
                 break;
@@ -65,8 +61,9 @@ public class PomocneMetode {
             }
         }
     }
-    static void loginNaMail(){
 
+    static void loginNaMail() {
+        //unosUsernameIPassword2();
         //unosUsernameIPassword();
     }
 }
